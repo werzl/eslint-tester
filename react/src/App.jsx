89b05df29de3634d774ed99af40216cf66e7463e
werzl <adam@houseofhewitt.co.uk>
2022-07-29 13:29:57 +0100

    include jsx-a11y and react-hooks

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,30 +1,47 @@
-import React from "react";
+import React, { useRef, useState, useEffect, useCallback } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 import TestComponent from "./TestComponent";
 
-function app() {
+const todos = [];
+
+function App() {
+    const [myState, setMyState] = useState("");
+    const myRef = useRef(null);
+
+    useEffect(() => {
+        setMyState(test());
+    }, [test]);
+
+    const test = useCallback(() => {
+        return "testy";
+    }, []);
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    Edit <code>src/App.js</code> and save to reload. {myState}
                 </p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
-                    hidden>
+                    ref={myRef}>
                     Learn React
                 </a>
             </header>
+
+            {todos.map((todo) =>
+                <div key={todo.id}>{todo}</div>
+            )}
 
             <TestComponent ShowDiv />
         </div>
     );
 }
 
-export default app;
+export default App;
